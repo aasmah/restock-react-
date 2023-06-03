8148eb60460171187b8ed0f4e24b2d1afddfc8d8
aasmah <aas.29980@gmail.com>
2023-06-03 14:52:55 -0500

    cart.jsx upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Products from "./cart2"
+
+
+ReactDOM.render(<Products />, document.getElementById("root"));
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header"></header>
+//       <Products />
+//     </div>
+//   );
+// }
+
+
+export default App;
